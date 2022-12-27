8bfd3b92b39fc6ea588f5f3efa2052c3020458ca
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-12-27 14:52:33 +0100

    feat: icons

---
+++ src/lib/icons/DefineIcons.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { register, symbols } from "./internal";
+</script>
+
+{#if register.size}
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" aria-hidden="true">
+        {#each Array.from(register) as icon}
+            {@const s = symbols[icon]}
+            <symbol id="icon-{icon}" viewBox="0 0 {s.size} {s.size}">
+                {@html s.body}
+            </symbol>
+        {/each}
+    </svg>
+{/if}
+
+<style>
+    svg {
+        transform: scale(0);
+        position: absolute;
+        opacity: 0;
+        height: 0;
+        width: 0;
+    }
+</style>
---
+++ src/lib/icons/Icon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { register, type Icon } from "./internal";
+    export let icon: Icon;
+    register.add(icon);
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+    <use fill="var(--icon-clr)" href="#icon-{icon}" width="48" height="48"></use>
+</svg>
+
+<style>
+    svg {
+        width: var(--icon-size, 1em);
+        height: var(--icon-size, 1em);
+        display: inline-block;
+    }
+</style>
