168559c7e1159bdca816966ff13e656930c3cf9c
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-12-27 14:55:09 +0100

    feat: about layout

---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import DefineIcons from "$lib/icons/DefineIcons.svelte";
+</script>
+
+<slot />
+<DefineIcons />
+
+<style>
+    @keyframes icon-shake {
+        0% { transform: rotate(0); }
+        25% { transform: rotate(-10deg); }
+        50% { transform: rotate(0); }
+        75% { transform: rotate(10deg); }
+        100% { transform: rotate(0); }
+    }
+</style>
