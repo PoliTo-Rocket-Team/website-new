be8e5bc8eb1216fc03f85ff2e971474516f457b5
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-01-18 12:42:01 +0100

    feat: projects page

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<main>
+    <h1>Projects</h1>
+    <div class="stop-growth">
+        <hr class="subtle">
+        <p>It's in the Team's goals to design and manufacture each year a new experimental rocket. In the 2022-2023 academic year, PoliTo Rocket Team has two active technical projects: Project Cavour and the Project Efesto. Project Cavour stands for the design and creation of PRT's first rocket, while Project Efesto concerns the research and development of a liquid rocket engine. In the upcoming months, the Team aims to start new projects to upgrade and modernize its technological capabilities, such as thrust vector control and active-fin control.</p>
+    </div>
+</main>
