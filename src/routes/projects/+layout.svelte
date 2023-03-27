a1b4dd45add0c1b26d5d0dd8b0d9135d4a56e6d0
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-03-27 17:09:21 +0200

    refactor: common projects markup

---
+++ src/routes/projects/+layout.svelte
@@ -0,0 +1,38 @@
+<main>
+    <div>
+        <slot />
+    </div>
+</main>
+
+<style lang="scss">
+
+    $space: 3.5rem;
+
+    div {
+        max-width: 90ch;
+        margin: 7rem auto;
+        --space: $space;
+
+        // Idk better way to have scoped common style
+
+        :global(h1) {
+            text-align: center;
+        }
+        :global(ul + p),
+        :global(p + p) {
+            margin-top: .7em;
+        }
+        :global(p + ul) {
+            margin-top: .3rem;
+        }
+        :global(.m-top) {
+            margin-top: $space;
+        }
+        :global(.list) {
+            padding-left: 3ch;
+        }
+        :global(.list > ::marker) {
+            color: var(--accent-fig);
+        }
+    }
+</style>
