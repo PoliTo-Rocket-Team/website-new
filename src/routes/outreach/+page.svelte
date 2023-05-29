70816aacefb65afa3334eb9e985b514982b1e067
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-05-29 13:23:48 +0200

    feat: outreach posts

--- src/routes/outreach/+page.svelte
+++ src/routes/outreach/+page.svelte
@@ -1,77 +1,105 @@
 <script>
     import Follow3D from "$lib/Follow3D.svelte";
+    import Masonry from "$lib/Masonry.svelte";
+    import Post from "./Post.svelte";
+    import posts from "./posts.json";
 </script>
 
 <main>
     <h1>Outreach</h1>
     <hr class="subtle">
-    <article aria-labelledby="iress">
-        <div class="with-flyer">
-            <div class="body blur-behind">
-                <h2 id="iress">Italian Rocket Engineering Student Summit</h2>
-                <p>PoliTo Rocket Team is pleased to invite you to the 1st IRESS &mdash; Italian Rocket Engineering Student Summit &mdash; that will be held on the 2<sup>nd</sup> of May 2023, from 3pm to 7pm in Aula Magna &ldquo;Giovanni Agnelli&rdquo; of Politecnico di Torino.</p>
-                <p>The IRESS will gather for the first time Rocket Teams from major Italian Universities in order to present an overview of Rocket Engineering realities and challenges, from the intercollegiate point of view and more.</p>
-                <p>Along with the students speeches, relevant personalities from aerospace companies will present their work as engineers concerning Propulsion and Rocket Engineering.</p>
+    <section aria-labelledby="events">
+        <h2 id="events">Events</h2>
+        <article class="sm-c" aria-labelledby="iress">
+            <div class="with-flyer">
+                <div class="body blur-behind">
+                    <h3 id="iress">Italian Rocket Engineering Student Summit</h3>
+                    <p>PoliTo Rocket Team is pleased to invite you to the 1st IRESS &mdash; Italian Rocket Engineering Student Summit &mdash; that will be held on the 2<sup>nd</sup> of May 2023, from 3pm to 7pm in Aula Magna &ldquo;Giovanni Agnelli&rdquo; of Politecnico di Torino.</p>
+                    <p>The IRESS will gather for the first time Rocket Teams from major Italian Universities in order to present an overview of Rocket Engineering realities and challenges, from the intercollegiate point of view and more.</p>
+                    <p>Along with the students speeches, relevant personalities from aerospace companies will present their work as engineers concerning Propulsion and Rocket Engineering.</p>
+                </div>
+                <div class="flyer">
+                    <Follow3D transform={(x,y) => `translate(${x*0.2}rem,${y*0.2}rem) rotate3d(${-y},${x},0,${-3*Math.hypot(x,y)}deg)`}>
+                        <img src="/img/IRESS.jpg" alt="Flyer IRESS" style="width: 15rem; max-width: 100%;">
+                    </Follow3D>
+                </div>
             </div>
-            <div class="flyer">
-                <Follow3D transform={(x,y) => `translate(${x*0.2}rem,${y*0.2}rem) rotate3d(${-y},${x},0,${-3*Math.hypot(x,y)}deg)`}>
-                    <img src="/img/IRESS.jpg" alt="Flyer IRESS" style="width: 15rem; max-width: 100%;">
-                </Follow3D>
+            <div class="split">
+                <ul class="center">
+                    <li>
+                        <span class="semi-bold">PoliTo Rocket Team</span><br>
+                        <span class="low">Politecnico di Torino</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">DARE</span><br>
+                        <span class="low"> Delft University of Technology</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">Skyward Experimental Rocketry</span><br>
+                        <span class="low">Politecnico di Milano</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">THRUST</span><br>
+                        <span class="low">Università degli Studi di Padova</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">Icarus</span><br>
+                        <span class="low">Politecnico di Torino</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">Unina Rockets</span><br>
+                        <span class="low">Università Federico II di Napoli</span>
+                    </li>
+                </ul>
+                <ul class="center">
+                    <li>
+                        <span class="semi-bold">Ing. Marino Fragnito</span><br>
+                        <span class="low">Senior Vice President Vega Business Unit at ArianeSpace</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">Ing. Francesca Romana Alvino</span><br>
+                        <span class="low">GNC engineer for VEGA launch vehicles at Avio</span>
+                    </li>
+                    <li>
+                        <span class="semi-bold">Davide Barbero</span><br>
+                        <span class="low">RCS Propulsion Lead Engineer for Space Rider Re-entry module</span>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="split">
-            <ul class="center">
-                <li>
-                    <span class="semi-bold">PoliTo Rocket Team</span><br>
-                    <span class="low">Politecnico di Torino</span>
-                </li>
-                <li>
-                    <span class="semi-bold">DARE</span><br>
-                    <span class="low"> Delft University of Technology</span>
-                </li>
-                <li>
-                    <span class="semi-bold">Skyward Experimental Rocketry</span><br>
-                    <span class="low">Politecnico di Milano</span>
-                </li>
-                <li>
-                    <span class="semi-bold">THRUST</span><br>
-                    <span class="low">Università degli Studi di Padova</span>
-                </li>
-                <li>
-                    <span class="semi-bold">Icarus</span><br>
-                    <span class="low">Politecnico di Torino</span>
-                </li>
-                <li>
-                    <span class="semi-bold">Unina Rockets</span><br>
-                    <span class="low">Università Federico II di Napoli</span>
-                </li>
-            </ul>
-            <ul class="center">
-                <li>
-                    <span class="semi-bold">Ing. Marino Fragnito</span><br>
-                    <span class="low">Senior Vice President Vega Business Unit at ArianeSpace</span>
-                </li>
-                <li>
-                    <span class="semi-bold">Ing. Francesca Romana Alvino</span><br>
-                    <span class="low">GNC engineer for VEGA launch vehicles at Avio</span>
-                </li>
-                <li>
-                    <span class="semi-bold">Davide Barbero</span><br>
-                    <span class="low">RCS Propulsion Lead Engineer for Space Rider Re-entry module</span>
-                </li>
-            </ul>
-        </div>
-    </article>
+        </article>
+    </section>
+    <section aria-labelledby="posts">
+        <h2 id="posts">Posts</h2>
+        <Masonry data={posts} let:date let:title let:img let:url let:body>
+            <Post {title} {date} {img} {url}>
+                <p>{body}</p>
+            </Post>
+        </Masonry>
+    </section>
 </main>
 
 <style>
     main {
-        max-width: 50rem;
+        max-width: 90ch;
         margin: 0 auto;
         padding: var(--pad);
     }
-    article {
-        margin-top: 8rem;
+    h2 {
+        font-size: calc(1.3 * var(--fs-70));
+        margin-bottom: 1em;
+    }
+    h3 {
+        font-size: var(--fs-65);
+        margin-bottom: 1rem;
+    }
+    section {
+        margin-top: 10rem;
+        --masonry-gap: 2rem;
+        --masonry-cols: repeat(auto-fill, minmax(28ch,1fr))
+    }
+    .sm-c {
+        max-width: 50rem;
+        margin-inline: auto;
     }
     .split,
     main p + p {
---
+++ src/routes/outreach/Post.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    export let date: string;
+    export let title: string;
+    export let img: string;
+    export let url: string;
+
+    const formatter = new Intl.DateTimeFormat("en-US", { day: "numeric", month: "long", year: "numeric" });
+</script>
+
+<article>
+    <a href={url} tabindex="-1" class="img" target="_blank" rel="noopener">
+        <img src={img} alt="Post image of {title}">
+    </a>
+    <div class="text">
+        <time datetime={date}>{formatter.format(new Date(date))}</time>
+        <h3>{title}</h3>
+        <div class="body">
+            <slot />
+        </div>
+        <a href={url} class="cta" target="_blank" rel="noopener">Read more</a>
+    </div>
+</article>
+
+<style>
+    article {
+        background-color: var(--bg-1);
+        border-radius: 0.5rem;
+    }
+    .img {
+        display: block;
+        border-radius: 0.5rem;
+        overflow: hidden;
+    }
+    .text {
+        padding: 1.2rem;
+        padding-top: 0.9rem;
+    }
+    img {
+        display: block;
+        max-width: 100%;
+        transform: scale(1);
+        transition: transform 0.2s ease;
+    }
+    img:hover {
+        transform: scale(1.05);
+    }
+    h3 {
+        font-size: var(--fs-65);
+        margin-bottom: .8rem;
+    }
+    .cta {
+        display: block;
+        width: fit-content;
+        margin-top: .8rem;
+        text-decoration: none;
+        padding: .4rem .8rem;
+        border: 2px solid var(--accent-fig);
+    }
+</style>
