6f7d8103d35680d7d80bace0f8a9153eae27f4f1
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-07-18 19:02:50 +0200

    feat: Expand component

---
+++ src/lib/Expand.svelte
@@ -0,0 +1,41 @@
+<script context="module">
+    import { browser } from "$app/environment";
+    if(browser) {
+        function csbw(){
+            document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+        }
+        if(document.readyState === "complete") csbw();
+        else {
+            window.addEventListener("DOMContentLoaded", csbw, false);
+            window.addEventListener("load", csbw);
+        }
+    }
+</script>
+
+<div class="container">
+    <div class="shift">
+        <div class="clip-zone">
+            <slot />
+        </div>
+    </div>
+</div>
+
+<style>
+    @media (min-width: 50rem) {
+        /* *{ outline: 1px solid red;} */
+        .container {
+            max-width: 100%;
+            position: relative;
+        }
+        .shift {
+            /* transform: translateX(calc(50% - 0.5*var(--size))); */
+            transform: translateX(calc(50% - 50vw + 0.5 * var(--scrollbar-width)));
+            width: 100%;
+            overflow-x: visible;
+        }
+        .clip-zone {
+            overflow: hidden;
+            width: calc(100vw - var(--scrollbar-width));
+        }
+    }
+</style>
