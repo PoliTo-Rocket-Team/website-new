82a51a53bddde035cd901e3f7bc3c27573998d33
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-14 19:39:52 +0200

    chore: import 600 weight

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,5 @@
 import "@fontsource/plus-jakarta-sans/400.css";
 import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/600.css";
 import "@fontsource/plus-jakarta-sans/700.css";
 import "$lib/global.css";
