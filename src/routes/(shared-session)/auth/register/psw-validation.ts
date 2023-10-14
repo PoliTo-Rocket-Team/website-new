bad613448a4467647ca5abd379ebaf86698798b6
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-14 15:49:30 +0200

    fix: psw check regexp

--- src/routes/(shared-session)/auth/register/psw-validation.ts
+++ src/routes/(shared-session)/auth/register/psw-validation.ts
@@ -8,7 +8,7 @@ export const pswCriteria = [
 const d = /\d/;
 const l = /[a-z]/;
 const u = /[A-Z]/;
-const s = /[\(\)\[\]\{\}.:;,!?@#€£$%^&*_-=+<>*|\\\/~`'"°§]/;
+const s = /[()[\]{}.:;,!?@#€£$%^&*_\-=+<>*|\\/~`'"°§]/;
 
 export function getPswCriteriaMask(v: string) {
     return 0 | +!(v.length >= 10) | (+!(d.test(v)) << 1) | (+!(l.test(v) && u.test(v)) << 2) | (+!(s.test(v)) << 3);
