2b555fc13772f7cc3f4e8c0390baa51d78ee71b2
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-14 19:32:35 +0200

    chore: public layouts

--- src/routes/(public)/+layout.server.ts
+++
@@ -1,3 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async ({ locals }) => ({ supabase: locals.supabase });
--- src/routes/(public)/+layout.ts
+++ src/routes/(public)/+layout.ts
@@ -1,4 +1,1 @@
 export const prerender = true;
-
-import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
