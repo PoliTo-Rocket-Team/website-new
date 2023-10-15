76dcd27b21e9dc1a39699f669187b342911242f5
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-15 18:37:46 +0200

    refactor: dashboard main page

--- src/routes/(shared-session)/dashboard/+page.svelte
+++ src/routes/(shared-session)/dashboard/+page.svelte
@@ -2,6 +2,7 @@
     import { enhance } from "$app/forms";
     import { goto } from "$app/navigation";
     import type { PageData } from "./$types";
+    import Follow3D from "$lib/Follow3D.svelte";
 
     export let data: PageData;
 
@@ -9,6 +10,8 @@
     function correct(v: string) {
         return v.toLowerCase().replace(space,'');
     }
+
+    const transform = (x: number, y: number) => `rotate3d(${-y},${x},0,${-15*Math.hypot(x,y)}deg)`;
 </script>
 
 <svelte:head>
@@ -18,8 +21,8 @@
 <main>
     {#if !data.person}
         <h1>Hi!</h1>
-        <p>It seems like there's no user data linked to your user id. If you're not a robot, you should notify the IT lead of your situation.</p>
-        <p style="margin-top: .5rem;">User ID: <b>{data.session?.user.id}</b><br>Email: <b>{data.session?.user.email}</b></p>
+        <p>It seems like there's still no user data linked to your user id. In the future the invitation process will be entirely automatic, but in the meantime please notify the IT lead of your situation.</p>
+        <p style="margin-top: .5rem;">User ID: <b>{data.user.id}</b><br>Email: <b>{data.user.email}</b></p>
         <form action="/auth/logout" method="post" use:enhance={async ({ cancel }) => {
             cancel();
             await data.supabase.auth.signOut();
@@ -28,118 +31,47 @@
             <button type="submit">Logout</button>
         </form>
     {:else}
-        {@const mail = `${correct(data.person.first_name)}.${correct(data.person.last_name)}@politorocketteam.it`}
         <h1>Welcome back {data.person.first_name}!</h1>
-        <div class="split">
-            <aside>
-                <ul class="links">
-                    <li><a href="#account">Account</a></li>
-                    <li><a href="#positions">Positions</a></li>
-                </ul>
-                <form action="/auth/logout" method="post" use:enhance={async ({cancel}) => {
-                    cancel();
-                    await data.supabase.auth.signOut();
-                    setTimeout(()=>goto("/auth/login"), 1);
-                }}>
-                    <button type="submit">Logout</button>
-                </form>
-            </aside>
-            <div class="body">
-                <section aria-labelledby="account">
-                    <h2 id="account">Account</h2>
-                    <span class="name">{data.person.first_name} {data.person.last_name}</span>
-                    <ul class="titles">
-                        {#each data.subteams as s}
-                            <li>{s.title}</li>
-                        {/each}
-                        {#each data.divisions as d}
-                            <li>{d.name} Lead {d.acting ? "(acting)":''}</li>
-                        {/each}
-                    </ul>
-                    <h3>Contacts</h3>
-                    <table>
-                        <tr>
-                            <td>Personal mail:</td>
-                            <td><a href="mailto:{data.session?.user.email}">{data.session?.user.email}</a></td>
-                        </tr>
-                        <tr>
-                            <td>Official mail:</td>
-                            <td><a href="mailto:{mail}">{mail}</a></td>
-                        </tr>
-                        <tr>
-                            <td>LinkedIn user:</td>
-                            <td>
-                                {#if data.person.linkedin}
-                                <a href="http://www.linkedin.com/in/{data.person.linkedin}" target="_blank">{data.person.linkedin}</a>
-                                {:else}
-                                <span class="low">none</span>
-                                {/if}
-                            </td>
-                        </tr>
-                    </table>
-                    <button type="button">Modify account</button>
-                </section>
-                <section aria-labelledby="account">
-                    <h2 id="account">Positions</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Division</th>
-                                <th>Name</th>
-                                <th>Code</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {#each data.positions as p}
-                                <tr>
-                                    <td>{p.division?.name}</td>
-                                    <td>{p.name}</td>
-                                    <td>{p.division?.code}-{p.number.toString().padStart(3,'0')}</td>
-                                </tr>
-                            {/each}
-                        </tbody>
-                    </table>
-                    <button type="button">Add new position</button>
-                </section>
-            </div>
+
+        <div class="cards">
+            <article>
+                <Follow3D {transform}>
+                    <a href="/dashboard/account">
+                        <img src={data.pic} alt="Hopefully your face">
+                        <h2>Account</h2>
+                    </a>
+                </Follow3D>
+            </article>
+            {#each data.divisions as d}
+                <article>
+                    <Follow3D {transform}>
+                        <a href="/dashboard/positions/{d.id}">
+                            <span class="water">{d.code}</span>
+                            <h2>{d.name}</h2>
+                        </a>
+                    </Follow3D>
+                </article>
+            {/each}
+            {#each data.subteams as s}
+                <article>
+                    <Follow3D {transform}>
+                        <a href="/dashboard/positions/{s.id}">
+                            <span class="water">{s.code}</span>
+                            <h2>{s.name}</h2>
+                        </a>
+                    </Follow3D>
+                </article>
+            {/each}
         </div>
     {/if}
 </main>
 
 <style>
-    main {
-        padding: max(1rem, 50vw - 45ch);
-    }
-    .split {
-        display: grid;
-        grid-template-columns: auto 1fr;
-        align-items: start;
-    }
-    aside {
-        text-align: right;
-        padding: 1rem 1.7rem;
-        border-right: 2px solid #888;
-        min-height: 40vh;
-        position: sticky;
-        top: 25vh;
-    }
-    .links {
-        list-style: none;
-        display: flex;
-        flex-direction: column;
-        align-items: end;
-    }
-    .links a {
-        text-decoration: none;
-        font-size: 1.3rem;
-        font-weight: 500;
-    }
-    .body {
-        padding: var(--pad);
-    }
-    section {
-        margin-top: 0;
-        margin-bottom: 7rem;
+    /* main {
+        padding:  max(1rem, 50vw - 45ch);
+    } */
+    h1 {
+        text-align: center;
     }
     button {
         color: var(--accent-text);
@@ -147,16 +79,89 @@
         border: 1px solid var(--accent-text);
         margin-top: 1rem;
     }
-    h3 {
-        margin-top: 1rem;
-        margin-bottom: .3rem;
+    article {
+        height: 20rem;
+        width: 20rem;
+        max-width: 100%;
+        margin-bottom: 2rem;
     }
-    .name {
+    a {
+        /* overflow: hidden; */
+        position: relative;
+        border-radius: 1.5rem;
+        border: 2px solid #8882;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        text-decoration: none;
+        color: inherit;
+        z-index: 1;
+    }
+    h2 {
+        text-align: center;
+        margin-bottom: 0;
+        z-index: 1;
+    }
+    .cards {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        --perspective: 30rem;
+    }
+    @media (min-width: 50rem) {
+        .cards {
+            flex-direction: row;
+            justify-content: center;
+        }
+        article {
+            margin: 2rem;
+        }
+
+        a {
+            /* perspective: var(--perspective); */
+            transform-style: preserve-3d;
+        }
+        h2, .water, img {
+            transition: transform .2s;
+        }
+        h2 {
+            transform: translateZ(0rem);
+        }
+        a:hover h2 {
+            transform: translateZ(4rem);
+        }
+        a:hover .water {
+            transform: translate3d(-50%, -50%, -4rem);
+        }
+        a:hover img {
+            transform: translateZ(-4rem);
+        }
+    }
+    img {
+        position: absolute;
+        inset: 10px;
+        width: calc(100% - 20px);
+        height: calc(100% - 20px);
+        object-fit: cover;
+        z-index: -1;
+        filter: blur(4px);
+        opacity: 0.7;
+        border-radius: calc(1.5rem - 10px);
+    }
+    .water {
+        z-index: -1;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate3d(-50%, -50%, 0rem);
         font-weight: 600;
-        font-size: 1.1rem;
-    }
-    .titles {
-        list-style: none;
-        color: var(--accent-text);
+        color: var(--accent-fig);
+        font-size: 12rem;
+        opacity: 0.5;
+        width: max-content;
     }
 </style>
