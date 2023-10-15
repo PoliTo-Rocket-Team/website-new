305603a1c037808965059354cdec638082ccc52d
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-15 14:00:41 +0200

    refcator: common data in layout load

--- src/routes/(shared-session)/dashboard/+layout.ts
+++ src/routes/(shared-session)/dashboard/+layout.ts
@@ -1,14 +1,16 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { LayoutLoad } from "./$types";
+import { session2user } from "$lib/supabase";
 
-export const load: LayoutLoad = async ({ parent }) => {
-    let { supabase, session } = await parent();
-    if(!session) {
-        session = (await supabase.auth.getSession()).data.session;
-        if(!session) throw redirect(303, "/auth/login");
+export const load: LayoutLoad = async ({ parent, url }) => {
+    let { supabase, user } = await parent();
+    if(!user) {
+        console.log("dashboard: no user");
+        const session = (await supabase.auth.getSession()).data.session;
+        if(!session) throw redirect(303, "/auth/login?redirect="+encodeURI(url.pathname));
+        else user = session2user(session);
     }
-    const user = session.user.id;
-    const res = await supabase.from("people").select("id, first_name, last_name, linkedin").eq("user", user);
+    const res = await supabase.from("people").select("id, first_name, last_name, linkedin").eq("user", user.id);
     if(res.error) {
         // console.log(session);
         throw error(500, {
@@ -19,16 +21,14 @@ export const load: LayoutLoad = async ({ parent }) => {
     if(res.data.length === 0) return {person: null}
     const person = res.data[0];
 
-    const data = await Promise.all([
+    const p = await Promise.all([
         supabase.from("divisions").select("name, code, acting:lead_acting, subteam:subteams(name,code)").eq("lead", person.id),
         supabase.from("subteams").select("title:title_name, code").eq("chief", person.id),
-        supabase.from("positions").select("id, name, number, division:divisions(name,code)")
     ]);
 
     return {
-        user, email: session.user.email, person, 
-        divisions: data[0].data!,
-        subteams: data[1].data!,
-        positions: data[2].data!,
+        user, person, 
+        divisions: p[0].data!,
+        subteams: p[1].data!,
     }
 };
