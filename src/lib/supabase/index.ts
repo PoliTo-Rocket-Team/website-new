0aa0797c36574b0cb834a817af8c204ee63447de
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-15 13:59:18 +0200

    feat: layout returns user instead of session

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,2 +1,13 @@
+import type { Session } from "@supabase/supabase-js";
+
 export * from "./types";
 export * from "./config";
+
+export function session2user(s: Session) {
+    return ({
+        id: s.user.id,
+        email: s.user.email,
+        phone: s.user.phone,
+        metadata: s.user.user_metadata
+    });
+}
--- src/routes/(shared-session)/+layout.ts
+++ src/routes/(shared-session)/+layout.ts
@@ -1,7 +1,7 @@
 import { createSupabaseLoadClient } from "@supabase/auth-helpers-sveltekit";
 import type { LayoutLoad } from "./$types";
 import { PUBLIC_SUPABASE_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
-import { cookieOptions, type Database } from "$lib/supabase";
+import { cookieOptions, session2user, type Database } from "$lib/supabase";
 
 export const prerender = false;
 
@@ -14,6 +14,6 @@ export const load: LayoutLoad = async ({ fetch, data, depends }) => {
     serverSession: data.session,
     cookieOptions
   })
-  const res = await supabase.auth.getSession();
-  return { supabase, session: res.data.session }
+  const session = (await supabase.auth.getSession()).data.session;
+  return { supabase, user: session ? session2user(session) : null }
 };
