177612f6ce30f38c2f40c5da08ff22e3d4634d67
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-28 17:08:18 +0200

    feat: member onboarding and resignation procedures

--- src/routes/(shared-session)/dashboard/(sections)/bureaucracy/+page.svelte
+++ src/routes/(shared-session)/dashboard/(sections)/bureaucracy/+page.svelte
@@ -6,10 +6,47 @@
 
 <h2>Procedures</h2>
 
-<h3>Member join</h3>
+<h3>New member onboarding</h3>
 
-<h3>Member quit</h3>
+<ol>
+    <li>The Leads sends the link to the <a href="https://docs.google.com/forms/d/e/1FAIpQLSes5438q7h43gOyUeb9B-PgbIcor2AyFeocyksHlzsorhOzBw/viewform" target="_blank">registry form</a> and the Non-Disclosure Agreement (NDA) document (already pre-signed by the PoliTo Rocket Team’s President) to the selected individuals.</li>
+    <li>The new member(s) must: fill out and submit the registry form; fill out, sign and send the <abbr title="Non-Disclosure Agreement">NDA</abbr> via email to the Recruitment Manager (RM) at <a href="mailto:recruitment@politorocketteam.it">recruitment@politorocketteam.it</a>, with <a href="mailto:info@politorocketteam.it">info@politorocketteam.it</a> in Carbon Copy (CC) to ensure proper receipt and record-keeping.</li>
+    <li>The <abbr title="Recruitment Manager">RM</abbr> verifies the correctness of the <abbr title="Non-Disclosure Agreement">NDA</abbr> and reviews the responses on the registry form. If both are correct and complete, the <abbr title="Recruitment Manager">RM</abbr> will: add the new members to the Team’s <a href="https://docs.google.com/spreadsheets/d/1Ncalg8rljAv3aFhQ62oZk2ghBPrLs1m4RgFd_32vCOA/edit?usp=sharing" target="_blank">registry sheet</a>, updating the records accordingly; store the signed NDA in the appropriate folder on Google Drive, for future reference. The NDA documents must be saved in PDF format and renamed <i>LASTNAME FIRSTNAME NDA.pdf</i>.</li>
+    <li>The <abbr title="Recruitment Manager">RM</abbr> send a welcome email to each new member. Such email includes a warm welcome message and provides the link to join the Team’s designated communication platform, the Discord server. The invitation link must be temporary, of maximum 7 days. The email must have <a href="mailto:info@politorocketteam.it">info@politorocketteam.it</a> in Carbon Copy (CC).</li>
+    <li>Once the new members have joined the Team’s Discord server, the <abbr title="Recruitment Manager">RM</abbr> will assign each them their correct role within the team.</li>
+    <li>The direct superior to the new members &ndash; typically their Division Lead &ndash; has the responsibility of sharing the necessary files and folders from all file sharing platforms used by the Team, such as Google Drive, with the new members.</li>
+</ol>
+
+<h3>Member resignation</h3>
+<ol>
+    <li>The member(s) who have decided to quit the PoliTo Rocket Team must initiate the process by officially notifying their direct superior within the Team, e.g. their Division Lead. This can be done either via email or through the designated communication platform agreed upon within the Team (Discord).</li>
+    <li>Upon receiving the notification, the direct superior of the quitter(s) will be responsible for informing the <abbr title="Recruitment Manager">RM</abbr> by email at <a href="mailto:recruitment@politorocketteam.it">recruitment@politorocketteam.it</a>it about the resignation(s), providing the necessary details, including the full name(s) of the quitter(s). </li>
+    <li>The <abbr title="Recruitment Manager">RM</abbr> will then take immediate action to remove the quitter(s) from the official Team’s <a href="https://docs.google.com/spreadsheets/d/1Ncalg8rljAv3aFhQ62oZk2ghBPrLs1m4RgFd_32vCOA/edit?usp=sharing" target="_blank">registry sheet</a> and from the Discord platform by kicking or banning them from the server.</li>
+    <li>The Lead who supervised the quitter(s) has the responsibility of removing the quitter(s) account(s) from all file sharing platforms used by the Team, such as Google Drive. The Lead must thoroughly review the shared files and directories to ensure that no confidential or sensitive information is being shared with the quitter(s) (a guide on how to find files that have been shared to a specific person on Google Drive can be found <a href="https://support.google.com/drive/answer/2375114?sjid=10290732754056045397-EU#zippy=%2Cfilter-your-drive-results" target="_blank">here</a>).</li>
+</ol>
 
 <h2>Regulations</h2>
 
 <h2>Documents</h2>
+
+
+<style>
+    h3 {
+        font-size: var(--fs-60);
+        margin-top: 2.5rem;
+        margin-bottom: 1rem;
+    }
+    ol {
+        padding-left: 2ch;
+    }
+    li {
+        margin-top: .5rem;
+    }
+    li::marker {
+        color: var(--accent-text);
+    }
+    abbr {
+        text-decoration: dashed underline 1px var(--accent-text);
+        text-underline-offset: 2px;
+    }
+</style>
