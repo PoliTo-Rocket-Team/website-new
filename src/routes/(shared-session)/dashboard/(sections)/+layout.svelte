336adef8be1c1eef4ed15666fdda9ecfe0f2685d
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-10-28 13:55:32 +0200

    fix: dashboard link typo

--- src/routes/(shared-session)/dashboard/(sections)/+layout.svelte
+++ src/routes/(shared-session)/dashboard/(sections)/+layout.svelte
@@ -49,7 +49,7 @@
                 </li>
             {/each}
             <li>
-                <a href="/dashboard/bureaucray" use:current>Bureaucray</a>
+                <a href="/dashboard/bureaucracy" use:current>Bureaucray</a>
             </li>
             <li>
                 <a href="/dashboard/account" use:current>Account</a>
