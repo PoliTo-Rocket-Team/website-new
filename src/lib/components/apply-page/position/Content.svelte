56b2b0d604aabd8ad81b34fd2555502f88027d92
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-12-06 19:50:51 +0100

    fix: content without forn link

--- src/lib/components/apply-page/position/Content.svelte
+++ src/lib/components/apply-page/position/Content.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
     export let description: string;
-    export let required: string[];
+    export let required: string[] | null;
     export let desirable: string[];
-    export let form: string;
+    export let form: string | null;
 </script>
 
 <h4>Description</h4>
 <p>{description}</p>
 
-{#if required.length}
+{#if required && required.length}
     <h4>Required skills</h4>
     <ul>
         {#each required as s}
@@ -24,9 +24,16 @@
     {/each}
 </ul>
 
-<a class="btn" href="https://forms.gle/{form}" target="_blank" rel="noreferrer"
-    >Apply</a
->
+{#if form}
+    <a
+        class="btn"
+        href="https://forms.gle/{form}"
+        target="_blank"
+        rel="noreferrer">Apply</a
+    >
+{:else}
+    <span>No google form is linked</span>
+{/if}
 
 <style>
     a {
@@ -40,5 +47,15 @@
     }
     a:focus {
         text-decoration: underline;
+    }
+    ul {
+        padding-left: 2ch;
+    }
+    h4:not(:first-child) {
+        margin-top: 1.5rem;
+        margin-bottom: 0.5rem;
+    }
+    p {
+        margin-bottom: 0.5rem;
     }
 </style>
