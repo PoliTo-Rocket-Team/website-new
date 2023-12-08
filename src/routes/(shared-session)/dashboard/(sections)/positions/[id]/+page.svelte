2b6e73a419ce6796b59cc5306c3a6a67587ba642
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-12-08 12:11:21 +0100

    refactor: position page

--- src/routes/(shared-session)/dashboard/(sections)/positions/[id]/+page.svelte
+++ src/routes/(shared-session)/dashboard/(sections)/positions/[id]/+page.svelte
@@ -1,206 +1,93 @@
 <script lang="ts">
-    import {
-        addPosition,
-        getPositions,
-        editPosition,
-        deletePosition,
-    } from "./databaseAPI";
-    import AdminPosition from "$lib/components/apply-page/AdminPosition.svelte";
-    import PositionForm from "$lib/components/apply-page/positionsForm.svelte";
-    import Addmodal from "$lib/components/apply-page/Addmodal.svelte";
+    import Modal from "$lib/Modal.svelte";
     import type { PageData } from "./$types";
-    import { onMount } from "svelte";
 
     import * as Position from "$lib/components/apply-page/position";
+    import { emptyPositionData } from "$lib/components/apply-page/position/validation";
+    import { setContext } from "svelte";
 
     export let data: PageData;
-    let positions = [];
-    let numbers = [];
-
+    setContext("supabase", data.supabase);
+    let deleting: { id: number; name: string } | null = null;
     let showAdd = false;
 
-    onMount(async () => {
-        await handelupdatePositions();
-    });
-    const handelupdatePositions = async () => {
-        const response = await getPositions(
-            data.supabase,
-            data.divisions[0].id
-        );
-        positions = response.data;
-        numbers = new Set(positions.map(position => position.number));
-    };
-    const handelsubmitAdd = async values => {
-        let res = await addPosition(values, data.supabase);
-        // let data = getPositions(data.supabase, data.divisions[0].id);
-        if (res.error) {
-            alert(res.error.message);
-        } else {
-            showAdd = false;
-            alert("Position added successfully");
-            await handelupdatePositions();
-        }
-    };
-    async function handelDelete(event) {
-        let id = event.detail.id;
+    let newPos = emptyPositionData(data.division.id);
+    $: newPos.number = (data.positions.length && data.positions[0].number) + 1;
 
-        const response = await deletePosition(id, data.supabase);
-        if (response.error) {
-            alert("Error deleting position!");
-        } else {
-            alert("Position deleted successfully");
-            await handelupdatePositions();
-        }
+    async function handelDelete() {
+        if (!deleting) return;
+        await data.supabase.from("positions").delete().eq("id", deleting.id);
+        deleting = null;
     }
-
-    const handelEditPositions = async values => {
-        const response = await editPosition(values, data.supabase);
-        if (response.error) {
-            alert("Error editing position!");
-        } else {
-            await handelupdatePositions();
-            alert("Position edited successfully");
-        }
-    };
 </script>
 
 <svelte:head>
-    <title>Positions | PRT Admin Program</title>
+    <title>{data.division.code} Positions | PRT Admin Program</title>
 </svelte:head>
 
-<h1>{data.division_name} positions</h1>
+<h1>{data.division.name} positions</h1>
 
-<Addmodal show={showAdd}>
-    <Position.Form creating />
-</Addmodal>
+<Modal bind:use={showAdd} empty={false}>
+    <h3>Add a new position</h3>
+    <Position.Form
+        creating
+        data={emptyPositionData(data.division.id)}
+        on:cancel={() => (showAdd = false)}
+        on:saved={e => {
+            console.log("saved");
+            data.positions.splice(0, 0, e.detail);
+            data.positions = data.positions;
+            showAdd = false;
+        }}
+    />
+</Modal>
 
-{#each data.positions as position (position.id)}
-    <Position.Admin data={position} code={data.code} />
+<Modal ch={32} bind:use={deleting} empty={{ id: -1, name: "" }} let:data>
+    <h3>Confirm deletion</h3>
+    <p>Are you sure you want to delete the position "{data.name}"?</p>
+    <p>This action cannot be undone later.</p>
+    <div class="btns">
+        <button type="button" class="btn btn--low" on:click={handelDelete}
+            >Delete</button
+        >
+        <button
+            type="button"
+            class="btn"
+            on:click={() => {
+                deleting = null;
+            }}>Cancel</button
+        >
+    </div>
+</Modal>
+
+{#each data.positions as position, i (position.id)}
+    <Position.Admin
+        data={position}
+        code={data.division.code}
+        on:saved={e => {
+            data.positions[i] = e.detail;
+        }}
+        on:delete={e => (deleting = e.detail)}
+    />
 {:else}
     <p>No positions found</p>
 {/each}
 
 <button
-    class="btn"
+    class="btn center"
     on:click={() => {
         showAdd = true;
     }}>Add a new position</button
 >
 
-<style lang="scss">
-    @use "$lib/components/apply-page/consts.scss" as *;
-
-    section {
-        max-width: 75ch;
-        margin-left: auto;
-        margin-right: auto;
+<style>
+    .btns {
+        display: flex;
+        justify-content: end;
+        column-gap: 1rem;
+        margin-top: 1rem;
     }
-
-    .code-label {
-        font-family: "Anonymous Pro", monospace;
-        width: 11ch;
-        max-width: 11ch;
-        min-width: 11ch;
-    }
-
-    $text-margin: 0.5rem;
-    .positions {
-        h4:not(:first-child) {
-            margin-top: 3 * $text-margin;
-        }
-        h4 {
-            margin-bottom: $text-margin;
-        }
-        p + ul {
-            margin-top: 0.5 * $text-margin;
-        }
-        ul + p,
-        p + p {
-            margin-top: $text-margin;
-        }
-        ul {
-            padding-left: 2ch;
-        }
-    }
-    .btn {
-        display: block;
-        padding: 0.4em 0.7em;
-        border: 2px solid var(--accent-fig);
-        width: fit-content;
-        text-decoration: none;
-        font-weight: 600;
+    .center {
         margin: 2rem auto;
     }
-    .btn:focus {
-        text-decoration: underline;
-    }
-    @media (min-width: 50rem) {
-        h2 {
-            text-align: center;
-        }
-        .position-labels {
-            padding: $details-pad-y $details-pad-x;
-            display: grid;
-            grid-template-columns: $details-columns;
-            grid-auto-flow: dense;
-            align-items: center;
-            column-gap: $gap;
-        }
-    }
-
-    // --------------------------------------------
-    .add-form-container {
-        padding: 1rem;
-        display: flex;
-        flex-direction: column;
-    }
-
-    form {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-    }
-    .border {
-        border-radius: 10px;
-    }
-
-    .input {
-        min-width: 100%;
-        min-height: 6rem;
-        box-sizing: border-box;
-        outline: none;
-        padding: 10px 10px 10px 10px;
-        resize: none;
-        margin-top: 0.5rem;
-    }
-
-    .short-input {
-        margin-left: 0.5rem;
-    }
-    .input-column {
-        display: flex;
-        gap: 1rem;
-        flex-wrap: wrap;
-    }
-    .number {
-        width: 3rem;
-        text-align: center;
-    }
-    .date {
-        padding: 0.1rem;
-    }
-    .short-input-border {
-        border-radius: 7px;
-    }
-
-    .form-btns-container {
-        display: flex;
-        justify-content: center;
-        gap: 1rem;
-    }
-
-    .error {
-        color: red;
-    }
-    // --------------------------------------------
 </style>
