3bac6c38af48f480491070779ffae7e86e773b3b
Fedesky25 <fedesky.guglielmi@gmail.com>
2023-12-08 11:57:46 +0100

    fix: position save function

--- src/lib/components/apply-page/position/validation.ts
+++ src/lib/components/apply-page/position/validation.ts
@@ -2,29 +2,45 @@ import type { Database } from "$lib/supabase";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import * as yup from "yup";
 
-export type PositionData = Omit<
-    Database["public"]["Tables"]["positions"]["Row"],
-    "division"
->;
+export type PositionData = Database["public"]["Tables"]["positions"]["Row"];
+
+export function emptyPositionData(division: number): PositionData {
+    return {
+        id: -1,
+        division,
+        name: "",
+        description: "",
+        desirable: [],
+        required: [],
+        number: 1,
+        form: "",
+        open: true,
+    };
+}
 
 export const fields = {
     name: yup.string().required().min(10),
-    number: yup.number().required().integer().moreThan(0),
     description: yup
         .string()
         .required()
         .min(40, "Must be more than 40 characters"),
     desirable: yup.array().required().of(yup.string().required()).min(1),
-    required: yup.array().of(yup.string().required()).nullable().defined(),
+    required: yup.array().of(yup.string().required()).defined().nullable(),
     form: yup
         .string()
-        .required()
+        .defined()
+        .nullable()
         .length(17, "Google form IDs are 17 characters long"),
     open: yup.boolean().required(),
-    // division: yup.number().required().min(1).integer(),
 };
 
-const whole = yup.object().shape(fields);
+const creationFields = {
+    number: yup.number().required().integer().moreThan(0),
+    division: yup.number().required().min(1).integer(),
+};
+
+const update = yup.object().shape(fields);
+const creation = yup.object().shape({ ...fields, ...creationFields });
 
 type SaveResult =
     | { data: null; errors: string[] }
@@ -36,44 +52,63 @@ export async function save(
 ): Promise<SaveResult> {
     const id = +(data.get("id") || 0);
     const name = data.get("name");
-    const number = +(data.get("number") || 0);
-    const description = data.get("description");
+    const description = data.get("description") || null;
     const desirable = data.getAll("desirable");
     const required = data.getAll("required");
-    const form = data.get("form");
+    const form = data.get("form") || null;
     const open = data.has("open");
 
     try {
-        const obj = await whole.validate(
-            {
-                name,
-                number,
-                description,
-                desirable,
-                required,
-                form,
-                open,
-            },
-            { abortEarly: false }
-        );
         if (id > 0) {
+            const obj = await update.validate(
+                {
+                    name,
+                    description,
+                    desirable,
+                    required,
+                    form,
+                    open,
+                },
+                { abortEarly: false }
+            );
             const res = await supabase
                 .from("positions")
                 .update(obj)
-                .eq("id", id);
-            return res.error
-                ? { data: null, errors: [res.error.message] }
-                : { data: Object.assign(obj, { id }), errors: [] };
+                .eq("id", id)
+                .select("id, number, division");
+            if (res.error) return { data: null, errors: [res.error.message] };
+            if (!res.data.length)
+                return { data: null, errors: ["No such position exists"] };
+            return {
+                errors: [],
+                data: Object.assign(obj, res.data[0]),
+            };
         } else {
+            const number = +(data.get("number") || 0);
             const division = +(data.get("division") || 0);
+            const obj = await creation.validate(
+                {
+                    name,
+                    number,
+                    division,
+                    description,
+                    desirable,
+                    required,
+                    form,
+                    open,
+                },
+                { abortEarly: false }
+            );
             if (!Number.isInteger(division) || division < 0)
                 return { data: null, errors: ["Invalid division"] };
+            const row = Object.assign(obj, { division });
             const res = await supabase
                 .from("positions")
-                .insert(Object.assign(obj, { division }))
+                .insert(row)
                 .select("id");
-            if (res.error) return { data: null, errors: [res.error.message] };
-            return { data: Object.assign(obj, res.data[0]), errors: [] };
+            return res.error
+                ? { data: null, errors: [res.error.message] }
+                : { data: Object.assign(row, res.data[0]), errors: [] };
         }
     } catch (err) {
         return { data: null, errors: (err as yup.ValidationError).errors };
