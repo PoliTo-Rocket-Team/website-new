b6f12f70126ecfb6413fab951c8643559108b0b2
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-10-05 16:48:18 +0200

    chore(dashboard): orders before bureaucracy

--- src/routes/(shared-session)/dashboard/+page.svelte
+++ src/routes/(shared-session)/dashboard/+page.svelte
@@ -66,33 +66,6 @@
             {/each}
             <article>
                 <Follow3D {transform}>
-                    <a href="/dashboard/bureaucracy">
-                        <svg
-                            width="800px"
-                            height="800px"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <g
-                                stroke-width="1.5"
-                                stroke-linecap="round"
-                                stroke="var(--accent-fig)"
-                            >
-                                <path d="M8 12H9M16 12H12" />
-                                <path d="M16 8H15M12 8H8" />
-                                <path d="M8 16H13" />
-                                <path
-                                    d="M3 14V10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H13C16.7712 2 18.6569 2 19.8284 3.17157C20.4816 3.82476 20.7706 4.69989 20.8985 6M21 10V14C21 17.7712 21 19.6569 19.8284 20.8284C18.6569 22 16.7712 22 13 22H11C7.22876 22 5.34315 22 4.17157 20.8284C3.51839 20.1752 3.22937 19.3001 3.10149 18"
-                                />
-                            </g>
-                        </svg>
-                        <h2>Bureaucracy</h2>
-                    </a>
-                </Follow3D>
-            </article>
-            <article>
-                <Follow3D {transform}>
                     <a href="/dashboard/orders">
                         <svg
                             width="30"
@@ -115,6 +88,33 @@
                             </g>
                         </svg>
                         <h2>Orders</h2>
+                    </a>
+                </Follow3D>
+            </article>
+            <article>
+                <Follow3D {transform}>
+                    <a href="/dashboard/bureaucracy">
+                        <svg
+                            width="800px"
+                            height="800px"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <g
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke="var(--accent-fig)"
+                            >
+                                <path d="M8 12H9M16 12H12" />
+                                <path d="M16 8H15M12 8H8" />
+                                <path d="M8 16H13" />
+                                <path
+                                    d="M3 14V10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H13C16.7712 2 18.6569 2 19.8284 3.17157C20.4816 3.82476 20.7706 4.69989 20.8985 6M21 10V14C21 17.7712 21 19.6569 19.8284 20.8284C18.6569 22 16.7712 22 13 22H11C7.22876 22 5.34315 22 4.17157 20.8284C3.51839 20.1752 3.22937 19.3001 3.10149 18"
+                                />
+                            </g>
+                        </svg>
+                        <h2>Bureaucracy</h2>
                     </a>
                 </Follow3D>
             </article>
