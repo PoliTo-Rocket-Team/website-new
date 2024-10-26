4340dec62c6d067522a8ab1d6cfdf5fbf6a9454b
Fedesky25 <fedesky.guglielmi@gmail.com>
2024-10-26 15:40:44 +0200

    refactor: order header

--- src/lib/components/order/EditorHeader.svelte
+++ src/lib/components/order/EditorHeader.svelte
@@ -1,45 +1,50 @@
 <script lang="ts">
     import "@fontsource/anonymous-pro/400.css";
-    export let name: string | null;
-    export let quantity: number | null;
-    export let price: number | null;
+    import type { Database } from "$lib/supabase";
+    import Icon from "$lib/icons/Icon.svelte";
+    export let name: string;
+    export let quantity: number;
+    export let price: number;
     export let requesterName: string | null;
-    export let status: string | null;
+    export let status: Database["public"]["Enums"]["request_status"];
 
     // export let total = price * quantity;
     $: total = price * quantity;
 </script>
-    <summary>
-        <h3>{name}</h3>
-        <h3>{quantity}</h3>
-        <h3>{price}€</h3>
-        <h3>{total}€</h3>
-        <h3>{requesterName}</h3>
-        <h3>{status}</h3>
-    </summary>
 
-<style lang="scss">
-    $marker-dim: 0.3rem;
+<summary>
+    <h3>{name}</h3>
+    <span class="requester">{requesterName}</span>
+    <span class="total">{total}€</span>
+    <Icon
+        title={status}
+        icon={status === "accepted"
+            ? "square_check"
+            : status === "rejected"
+              ? "square_cross"
+              : "square_empty"}
+    />
+</summary>
 
+<style>
     summary {
-        padding: 1rem;
+        padding: 0.8rem 1rem;
         display: grid;
-        grid-template-columns: repeat(6, 1fr) $marker-dim;
+        grid-template-columns: 2fr 10ch 1fr auto 0.3rem;
         align-items: center;
-        column-gap: 1rem;
+        column-gap: 1.5rem;
         white-space: nowrap;
+        --icon-size: 1.5rem;
+    }
+    .total {
+        text-align: right;
+    }
 
-        
-    }
-    summary h3:last-child {
-        margin-left: auto; 
-        padding: 0; 
-    }
-    summary h3:first-child {
-        min-width: 10rem; 
-        white-space: nowrap; 
+    h3 {
+        min-width: 10rem;
+        white-space: nowrap;
         overflow: hidden;
-        text-overflow: ellipsis; 
+        text-overflow: ellipsis;
     }
     summary::-webkit-details-marker {
         display: none;
@@ -48,9 +53,9 @@
         content: "";
         width: 0;
         height: 0;
-        border-right: $marker-dim solid currentColor;
-        border-top: $marker-dim solid transparent;
-        border-bottom: $marker-dim solid transparent;
+        border-right: 0.3rem solid currentColor;
+        border-top: 0.3rem solid transparent;
+        border-bottom: 0.3rem solid transparent;
         transform: translateX(-1px);
         transition: transform 150ms;
     }
