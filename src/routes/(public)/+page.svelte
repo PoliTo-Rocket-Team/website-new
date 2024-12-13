a3f6d6b8d93a288f80581550df6c65d876681497
Huseyin Gulec <itshuseyingulec@gmail.com>
2024-12-13 01:47:41 +0100

    chore: add prt-polito logo to main page

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -53,6 +53,15 @@
     onDestroy(
         () => $content && $content.removeEventListener("scroll", onScroll)
     );
+
+    let currentTheme: string | null = null;
+
+    const unsubscribe = theme.subscribe(value => {
+        currentTheme = value;
+    });
+    onDestroy(() => {
+        unsubscribe();
+    });
 </script>
 
 <svelte:window on:resize={throttle(20, onResize)} />
@@ -83,7 +92,24 @@
 </header>
 <main bind:this={main}>
     <section aria-labelledby="identity-title">
+        <div class="logos blur-behind" >
+            <a href="https://www.polito.it/" target="_blank">
+                <img
+                    src={currentTheme === "dark"
+                        ? "/img/sponsors/poli.svg"
+                        : "/img/sponsors/poli-dark.svg"}
+                    alt="Politecnico di Torino logo"
+                />
+            </a>
+            <img
+                src={currentTheme === "dark"
+                    ? "/img/PRT-logo-horizontal.png"
+                    : "/img/PRT-logo-horizontal-dark.png"}
+                alt="Politecnico di Torino logo"
+            />
+        </div>
         <h2 id="identity-title">Who are we?</h2>
+
         <div class="blur-behind">
             <p>
                 We are the rocket engineering student team of Politecnico di
@@ -109,15 +135,38 @@
                 class="float"
             />
             <p>
-                PoliTo Rocket Team was the first team from Politecnico di Torino to compete at the European Rocketry Challenge (<a href="https://euroc.pt/">EuRoC</a>) in October 2023, launching the <a href="https://www.politorocketteam.it/projects/Cavour">Cavour</a> rocket and winning the ANACOM award!
+                PoliTo Rocket Team was the first team from Politecnico di Torino
+                to compete at the European Rocketry Challenge (<a
+                    href="https://euroc.pt/">EuRoC</a
+                >) in October 2023, launching the
+                <a href="https://www.politorocketteam.it/projects/Cavour"
+                    >Cavour</a
+                > rocket and winning the ANACOM award!
             </p>
 
             <p>
-                We competed again in 2024 with <a href="https://www.politorocketteam.it/projects/VES">Project VES</a>, our biggest rocket. The Vittorio Emanuele II Rocket (VES) was launched on October 12 at 14:19 (UTC+1). The flight was nominal, reaching an apogee of approximately 3160m. The drogue parachute deployed at apogee, reducing the rocket’s descent velocity to about 30m/s. The main parachute separation was successful; however the upper and lower rocket sections, each equipped with their own parachute, were separated during the descent. Both sections were located and recovered, and VES is now back in Turin ready to be reused.
+                We competed again in 2024 with <a
+                    href="https://www.politorocketteam.it/projects/VES"
+                    >Project VES</a
+                >, our biggest rocket. The Vittorio Emanuele II Rocket (VES) was
+                launched on October 12 at 14:19 (UTC+1). The flight was nominal,
+                reaching an apogee of approximately 3160m. The drogue parachute
+                deployed at apogee, reducing the rocket’s descent velocity to
+                about 30m/s. The main parachute separation was successful;
+                however the upper and lower rocket sections, each equipped with
+                their own parachute, were separated during the descent. Both
+                sections were located and recovered, and VES is now back in
+                Turin ready to be reused.
             </p>
 
             <p>
-                With VES, the Team was at its 2nd EuRoC participation and ranked 4th out of 20 for vehicle quality, and 6th out of 20 participating teams and out of 55 total teams in the general ranking. PRT received a larger score than some of Europe’s most experienced teams, and improved on the 2023 performance. It was also the highest score ever from a team from Politecnico di Torino.
+                With VES, the Team was at its 2nd EuRoC participation and ranked
+                4th out of 20 for vehicle quality, and 6th out of 20
+                participating teams and out of 55 total teams in the general
+                ranking. PRT received a larger score than some of Europe’s most
+                experienced teams, and improved on the 2023 performance. It was
+                also the highest score ever from a team from Politecnico di
+                Torino.
             </p>
         </div>
     </section>
@@ -156,11 +205,13 @@
                 (3,000 and 9,100 m).
             </p>
             <p>
-                PoliTo Rocket Team competed with the <a
-                    href="/projects/Cavour">Cavour rocket</a
+                PoliTo Rocket Team competed with the <a href="/projects/Cavour"
+                    >Cavour rocket</a
                 > in the 10 ft apogee AGL COTS category with a target apogee of around
                 3km.The Team ranked 13th in its category and 20th overall with 119
-                teams participating and over 150 total teams. PRT also won the prestigious Dr. Gil Moore Technical Award for Innovation, one of the few technical awards, for its extensive use of 3D-printed internal components!
+                teams participating and over 150 total teams. PRT also won the prestigious
+                Dr. Gil Moore Technical Award for Innovation, one of the few technical
+                awards, for its extensive use of 3D-printed internal components!
             </p>
         </div>
     </section>
@@ -172,8 +223,9 @@
             </div>
             <div class="timeline__body">
                 <Event year={2021} month={10} ch={45} title="The beginning"
-                    >Inspired by American and European rocketry teams, a group of friends started designing an experimental
-                    sounding rocket.</Event
+                    >Inspired by American and European rocketry teams, a group
+                    of friends started designing an experimental sounding
+                    rocket.</Event
                 >
                 <Event
                     year={2022}
@@ -203,7 +255,8 @@
                     ch={35}
                     title="First Cavour launch"
                 >
-                    Germany, 19:53:42 CET: test launch successful! First launch of a rocket built by students of Politecnico di Torino
+                    Germany, 19:53:42 CET: test launch successful! First launch
+                    of a rocket built by students of Politecnico di Torino
                     <a
                         href="https://www.instagram.com/p/CroYBp9t8Sr/?igshid=ZWIzMWE5ZmU3Zg=="
                         target="_blank"
@@ -251,28 +304,42 @@
                         href="https://euroc.pt/"
                         target="_blank"
                         rel="noreferrer">EuRoC</a
-                    >, Europe’s major rocket engineering competition, winning the ANACOM award.
+                    >, Europe’s major rocket engineering competition, winning
+                    the ANACOM award.
                 </Event>
                 <Event
                     year={2024}
                     month={10}
                     ch={45}
                     title="2024 European Rocketry Challenge"
-                    >At our second participation we launched <a href="https://www.politorocketteam.it/projects/VES">VES </a>, our biggest rocket, improving our past year performance, ranking 4th in Europe for for vehicle design and 6th overall.
+                    >At our second participation we launched <a
+                        href="https://www.politorocketteam.it/projects/VES"
+                        >VES
+                    </a>, our biggest rocket, improving our past year
+                    performance, ranking 4th in Europe for for vehicle design
+                    and 6th overall.
                 </Event>
                 <Event
                     year={2024}
                     month={10}
                     ch={45}
                     title="International Astronautical Congress 2024"
-                    >At the world’s largest IAC, held in Milan, PRT showcased its projects and students presented multiple technical papers. PRT won the Geography, Generation and Gender+ award at the Student Challenge.
+                    >At the world’s largest IAC, held in Milan, PRT showcased
+                    its projects and students presented multiple technical
+                    papers. PRT won the Geography, Generation and Gender+ award
+                    at the Student Challenge.
                 </Event>
                 <Event
                     year={2025}
                     month={6}
                     ch={45}
                     title="International Rocket Engineering Competition 2025"
-                    >With an evolution of <a href="https://www.politorocketteam.it/projects/VES">Project VES </a>, we aim to represent Italy and Politecnico di Torino once again at the world's largest rocketry competition, and aim for the Italian altitude record!
+                    >With an evolution of <a
+                        href="https://www.politorocketteam.it/projects/VES"
+                        >Project VES
+                    </a>, we aim to represent Italy and Politecnico di Torino
+                    once again at the world's largest rocketry competition, and
+                    aim for the Italian altitude record!
                 </Event>
             </div>
         </div>
@@ -678,6 +745,19 @@
         }
     }
 
+    .logos {
+            margin: 3rem auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+    .logos img {
+        width: 16rem;
+        margin: 1rem;
+    }
+
     @media (max-width: 50rem) {
         header h1,
         header p {
@@ -686,6 +766,8 @@
         header button {
             margin-bottom: 1.5rem;
         }
+
+       
     }
     @media (min-width: 50rem) {
         canvas {
@@ -700,6 +782,14 @@
         .timeline {
             padding-left: 0.5ch;
             // column-gap: max(1rem, 3vw);
+        }
+        
+        .logos {
+            flex-direction: row;
+        }
+        .logos img {
+            width: 20rem;
+            margin: 5rem 2rem;
         }
     }
 </style>
